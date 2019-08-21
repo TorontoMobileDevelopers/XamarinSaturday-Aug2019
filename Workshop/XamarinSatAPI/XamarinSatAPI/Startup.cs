6f4de384da62b473c4da75c5ba15438268b0f0f3
Daniel Causer <danielcauser@gmail.com>
2019-08-21 19:10:28 -0400

    APi small changes

--- Workshop/XamarinSatAPI/XamarinSatAPI/Startup.cs
+++ Workshop/XamarinSatAPI/XamarinSatAPI/Startup.cs
@@ -22,7 +22,7 @@ namespace XamarinSatAPI
         {
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("ToDoItemList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" }); });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "Xamarin Saturday", Version = "v1" }); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
