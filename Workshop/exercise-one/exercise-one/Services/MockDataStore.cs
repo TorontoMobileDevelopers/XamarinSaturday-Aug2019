d22367e3ec56df30a068414de7917c7e4f43c7a2
Daniel Causer <danielcauser@gmail.com>
2019-08-23 15:44:46 -0400

    exercise one completed

---
+++ Workshop/exercise-one/exercise-one/Services/MockDataStore.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using exercise_one.Models;
+
+namespace exercise_one.Services
+{
+    public class MockDataStore : IDataStore<Item>
+    {
+        List<Item> items;
+
+        public MockDataStore()
+        {
+            items = new List<Item>();
+            var mockItems = new List<Item>
+            {
+                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description.", Url = "http://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Portrait_Of_A_Baboon.jpg/314px-Portrait_Of_A_Baboon.jpg"},
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description.", Url = "http://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"},
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description.", Url = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"},
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description.", Url = "http://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg/220px-Saimiri_sciureus-1_Luc_Viatour.jpg" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description.", Url = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Golden_lion_tamarin_portrait3.jpg/220px-Golden_lion_tamarin_portrait3.jpg" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description.", Url = "http://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Alouatta_guariba.jpg/200px-Alouatta_guariba.jpg" }
+            };
+
+            foreach (var item in mockItems)
+            {
+                items.Add(item);
+            }
+        }
+
+        public async Task<bool> AddItemAsync(Item item)
+        {
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(Item item)
+        {
+            var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(oldItem);
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
+            items.Remove(oldItem);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Item> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
+}
---
+++ Workshop/exercise-one/exercise-one/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using exercise_one.Models;
+
+namespace exercise_one.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
